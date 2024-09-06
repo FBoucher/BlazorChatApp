be64f9d52f01c54305c61e121f730621352ce7ba
Frank <fboucher@outlook.com>
2024-09-06 16:04:09 -0400

    it's working in Container

--- BlazorChatApp/Program.cs
+++ BlazorChatApp/Program.cs
@@ -1,36 +1,43 @@
-using Azure.Identity;
 using BlazorChatApp;
 using BlazorChatApp.Components;
 using Microsoft.SemanticKernel;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Aspire update
 builder.AddServiceDefaults();
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+// Add Semantic Kernel
 builder.Services.AddKernel();
 
+// Add DetailedErrors
+builder.Services.AddServerSideBlazor().AddCircuitOptions(option => { option.DetailedErrors = true; });
+
+// Use this when Azure OpenAI
 // var aiConfig = builder.Configuration.GetSection("SmartComponents");
 // builder.Services.AddAzureOpenAIChatCompletion(
 //     deploymentName: aiConfig["DeploymentName"]!,
 //     endpoint: aiConfig["Endpoint"]!,
 //     new DefaultAzureCredential());
 
+// Use this when LLM is local
 #pragma warning disable SKEXP0003, SKEXP0010, SKEXP0011, SKEXP0052
 
-var aiConfig = builder.Configuration.GetSection("LocalSmart");
 builder.Services.AddOpenAIChatCompletion(
     modelId: "llama3.1",
-    endpoint: new Uri("http://localhost:11434/"),
+    endpoint: new Uri(builder.Configuration["LOCAL_LLM"] ?? "http://localhost:11434/"),
     apiKey: "apikey");
 
-builder.Services.AddScoped(sp => KernelPluginFactory.CreateFromType<ThemePlugin>(serviceProvider: sp));
+//builder.Services.AddScoped(sp => KernelPluginFactory.CreateFromType<ThemePlugin>(serviceProvider: sp));
+
 
 var app = builder.Build();
 
+// Aspire update
 app.MapDefaultEndpoints();
 
 // Configure the HTTP request pipeline.
