f57d8a9b8b2bb8c33abd30f18ed8484147b12e03
Frank <fboucher@outlook.com>
2024-09-11 13:50:46 -0400

    it's working now

--- BlazorChatApp/Program.cs
+++ BlazorChatApp/Program.cs
@@ -1,3 +1,4 @@
+using Azure.Identity;
 using BlazorChatApp;
 using BlazorChatApp.Components;
 using Microsoft.SemanticKernel;
@@ -18,19 +19,24 @@ builder.Services.AddKernel();
 builder.Services.AddServerSideBlazor().AddCircuitOptions(option => { option.DetailedErrors = true; });
 
 // Use this when Azure OpenAI
-// var aiConfig = builder.Configuration.GetSection("SmartComponents");
-// builder.Services.AddAzureOpenAIChatCompletion(
-//     deploymentName: aiConfig["DeploymentName"]!,
-//     endpoint: aiConfig["Endpoint"]!,
-//     new DefaultAzureCredential());
+var aiConfig = builder.Configuration.GetSection("SmartComponents");
+//builder.Services.AddAzureOpenAIChatCompletion(
+//    deploymentName: aiConfig["DeploymentName"]!,
+//    endpoint: aiConfig["Endpoint"]!,
+//    new DefaultAzureCredential());
+
+builder.Services.AddAzureOpenAIChatCompletion( 
+    deploymentName: aiConfig["DeploymentName"]!,
+    endpoint: aiConfig["Endpoint"]!,
+    apiKey: aiConfig["ApiKey"]!);
 
 // Use this when LLM is local
-#pragma warning disable SKEXP0003, SKEXP0010, SKEXP0011, SKEXP0052
+//#pragma warning disable SKEXP0003, SKEXP0010, SKEXP0011, SKEXP0052
 
-builder.Services.AddOpenAIChatCompletion(
-    modelId: "llama3.1",
-    endpoint: new Uri(builder.Configuration["LOCAL_LLM"] ?? "http://localhost:11434/"),
-    apiKey: "apikey");
+//builder.Services.AddOpenAIChatCompletion(
+//    modelId: "llama3.1",
+//    endpoint: new Uri(builder.Configuration["LOCAL_LLM"] ?? "http://localhost:11434/"),
+//    apiKey: "apikey");
 
 //builder.Services.AddScoped(sp => KernelPluginFactory.CreateFromType<ThemePlugin>(serviceProvider: sp));
 
