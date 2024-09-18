3794a15b037544744ce47663af7d3c5b5e6b0f22
Frank <fboucher@outlook.com>
2024-09-18 17:52:06 -0400

    demo rc

--- AppHost/Program.cs
+++ AppHost/Program.cs
@@ -1,5 +1,19 @@
+using Aspire.Hosting;
+using k8s.Models;
+using Microsoft.Extensions.Hosting;
+using Projects;
+using System.Data;
+using System.Net.Sockets;
+using System.Text.RegularExpressions;
+using static System.Net.WebRequestMethods;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.BlazorChatApp>("blazorchatapp");
+builder.AddProject<Projects.BlazorChatApp>("blazorchatapp")
+            .WithEnvironment("OTEL_SERVICE_NAME", "blazorchatapp")
+            .WithEnvironment("OTEL_EXPORTER_OTLP_ENDPOINT", "http://localhost:4317")
+            .WithEnvironment("OTEL_EXPORTER_OTLP_PROTOCOL", "grpc")
+            .WithEnvironment("OTEL_RESOURCE_ATTRIBUTES", "deployment.environment = docker,host.name = otelcol - docker")
+            .WithEnvironment("DD_AGENT_HOST", "datadog");
 
 builder.Build().Run();
